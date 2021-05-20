a50db8c10e89d2e2cf2a928d36dd8791524dd85d
juanDev-47 <juanp.arenas@udea.edu.co>
2021-05-19 22:55:01 -0500

    first commit

---
+++ js/interfaz.js
@@ -0,0 +1,7 @@
+export const formulario = document.querySelector('#formulario-buscar'),
+             buscar = document.querySelector('#buscar'),
+             divMensajes = document.querySelector('#mensajes'),
+             divResultado = document.querySelector('#resultado'),
+             headingResultado = document.querySelector('#titulo');
+
+
